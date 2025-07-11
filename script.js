758747c1c9acf9dbbe6b8323c032f85208274ab5
emma-nuel1708 <olanuelemmayemi@gmail.com>
2025-07-11 14:45:07 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollToTopBtn.classList.add("show");
+  } else {
+    scrollToTopBtn.classList.remove("show");
+  }
+});
+
+scrollToTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+function toggleMenu() {
+  const nav = document.getElementById("navlinks");
+  nav.classList.toggle("show");
+}
