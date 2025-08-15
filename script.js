e14a524cc059e0bc333027d706158ebd99d50760
emma-nuel1708 <olanuelemmayemi@gmail.com>
2025-08-15 17:36:29 +0100

    edited

--- script.js
+++ script.js
@@ -19,3 +19,17 @@ function toggleMenu() {
   const nav = document.getElementById("navlinks");
   nav.classList.toggle("show");
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.getElementById("hamburger");
+  const navlinks = document.getElementById("navlinks");
+  const closeBtn = document.getElementById("closeBtn");
+
+  hamburger.addEventListener("click", function () {
+    navlinks.classList.add("show");
+  });
+
+  closeBtn.addEventListener("click", function () {
+    navlinks.classList.remove("show");
+  });
+});
